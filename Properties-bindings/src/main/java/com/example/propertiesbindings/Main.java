1b298c6b6f447e65d19d2f612288ba2727f1daba
alexander.oliveira <alexander.oliveira99@gmail.com>
2024-10-03 15:06:52 +0100

    I was learning more about Observable and Binding

--- Properties-bindings/src/main/java/com/example/propertiesbindings/Main.java
+++ Properties-bindings/src/main/java/com/example/propertiesbindings/Main.java
@@ -5,10 +5,14 @@ import com.example.propertiesbindings.model.Employeee;
 import javafx.application.Application;
 import javafx.beans.property.ReadOnlyIntegerProperty;
 import javafx.beans.property.ReadOnlyIntegerWrapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -16,9 +20,11 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
         VBox root = new VBox();
-        Employeee employeee = new Employeee("Alexander Oliveira", 100);
         root.setAlignment(Pos.CENTER);
         root.setSpacing(10);
+
+        //TODO: Reading a Properties such as here...
+        Employeee employeee = new Employeee("Alexander Oliveira", 100);
 
         Label label = new Label();
         Button button = new Button("Double Salary");
@@ -30,7 +36,6 @@ public class Main extends Application {
                 " | Salary: " + employeee.getSalary()));
         root.getChildren().addAll(label, button);
 
-        //TODO: Reading a Properties such as here...
         ReadOnlyIntegerWrapper numWrapper = new ReadOnlyIntegerWrapper(100);
         ReadOnlyIntegerProperty num = numWrapper.getReadOnlyProperty();
 
@@ -47,8 +52,34 @@ public class Main extends Application {
         constant.changeStringWrapper();
         System.out.println(constant.getReadOnlyString().get());
 
+        // TODO : Building example
+        Label labelText = new Label("Welcome the Java FX");
+        TextField textField = new TextField();
+        Button button1 = new Button("Bind Label TO Field");
+        ///TextField textField1 = new TextField();
+        textField.setMaxWidth(200);
+        button1.setOnAction(actionEvent -> labelText.textProperty().bind(textField.textProperty()));
+        ///labelText.textProperty().bind(textField.textProperty());
+        ///textField.textProperty().addListener((observable, oldVal, newVal) -> labelText.setText(newVal));
+        labelText.textProperty().bind(textField.textProperty());
+        root.getChildren().addAll(labelText, textField, button1);
 
-        Scene scene = new Scene(root, 550, 500);
+        //TODO Observable Collections example
+        ObservableList<String> observableList =
+                FXCollections.observableArrayList("Palmeiras 99", "Palmeiras 20", "Palmeiras 21");
+        ListView<String> items = new ListView<>();
+        TextField textField1 = new TextField();
+        textField1.setMaxWidth(200);
+        Button button2 = new Button("Add new Palmeiras title here: ");
+        items.setItems(observableList);
+        button2.setOnAction(observable -> {
+            if(!textField.textProperty().get().isEmpty()){
+                observableList.add(textField1.getText());
+            }
+        });
+
+        root.getChildren().addAll(items, textField1, button2);
+        Scene scene = new Scene(root, 700, 700);
         stage.setScene(scene);
         stage.show();
     }
